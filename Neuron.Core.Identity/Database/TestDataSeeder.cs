124c0a7d006a0071a0ff3beb6c4ce5f56413f33f
juliangiebel <juliangiebel@live.de>
2025-08-07 21:28:17 +0200

    a

--- Neuron.Core.Identity/Database/TestDataSeeder.cs
+++ Neuron.Core.Identity/Database/TestDataSeeder.cs
@@ -11,9 +11,47 @@ public sealed class TestDataSeeder(IServiceProvider serviceProvider) : IHostedSe
     {
         using var scope = serviceProvider.CreateScope();
         
+        await PopulateRolesAsync(scope);
         await PopulateUsersAsync(scope);
+        await AssignRolesAsync(scope);
     }
-    
+
+    private async ValueTask AssignRolesAsync(IServiceScope scope)
+    {
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdpUser>>();
+        
+        var user = await userManager.FindByNameAsync("TestUser");
+        if (user == null) 
+            return;
+
+        var roles = new[] { "Admin", "User", "Moderator" };
+        foreach (var role in roles)
+        {
+            if (!await userManager.IsInRoleAsync(user, role))
+                await userManager.AddToRoleAsync(user, role);
+        }
+    }
+
+    private async ValueTask PopulateRolesAsync(IServiceScope scope)
+    {
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdpRole>>();
+        
+        var roles = new[]
+        {
+            "Admin",
+            "User",
+            "Moderator"
+        };
+
+        foreach (var roleName in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdpRole { Name = roleName });
+            }
+        }
+    }
+
     public Task StopAsync(CancellationToken cancellationToken)
     {
         return Task.CompletedTask;
---
+++ Neuron.OpenId/Services/Interfaces/IAuthIdentityService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace Neuron.Core.OpenId.Services.Interfaces;
+
+public interface IIdentityClaimsService
+{
+    Task<string> GetUserIdAsync();
+    Task<IEnumerable<Claim>> GetClaimsAsync();
+    Task<bool> CanSignInAsync();
+}
